281da23963f85550839f5882f224956a00567879
Jwall1103 <jwalla30@dtcc.edu>
2023-01-26 00:03:03 -1000

    third commit, updated collisions + animation

--- src/scenes/LevelScene.ts
+++ src/scenes/LevelScene.ts
@@ -1,53 +1,72 @@
 import components from "../components";
 
 export default class LevelScene extends Phaser.Scene {
-  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
-  private fauna!: Phaser.Physics.Arcade.Sprite
 
   constructor() {
     super({ key: "LevelScene" });
   }
+  private blockMap: Map<string, number[]> = new Map();
+  private fauna!: Phaser.Physics.Arcade.Sprite
+
+  tileSize = 32;
+	scoreText!: Phaser.GameObjects.Text
+  movesLeft!: Phaser.GameObjects.Text 
 
   preload() {
-    this.cursors = this.input.keyboard.createCursorKeys()
+    this.load.image("tiles", "assets/drawtiles-spaced.png");
+    this.load.image('whenGo', 'assets/whenGoClicked.png')
+    this.load.image('forward1', 'assets/Forward.png')
+    this.load.image('forward2', 'assets/Forward.png')
+    this.load.image('forward3', 'assets/Forward.png')
+    this.load.image('forward4', 'assets/Forward.png')
+    this.load.image('forward5', 'assets/Forward.png')
+    this.load.image('forward6', 'assets/Forward.png')
+    this.load.image('forward7', 'assets/Forward.png')
+    this.load.image('forward8', 'assets/Forward.png')
+    this.load.image('forward9', 'assets/Forward.png')
+    this.load.image('forward10', 'assets/Forward.png')
+    this.load.image('right1', 'assets/RightTurn.png')
+    this.load.image('right2', 'assets/RightTurn.png')
+    this.load.image('right3', 'assets/RightTurn.png')
+    this.load.image('left1', 'assets/LeftTurn.png')
+    this.load.image('left2', 'assets/LeftTurn.png')
+    this.load.image('left3', 'assets/LeftTurn.png')
+    this.load.image('go', 'assets/GoButton.png')
+    this.load.image('down', 'assets/Down.png')
     this.load.image('outdoor-tiles', 'assets/tiles/outdoors.png')
     this.load.image('vehicle-tiles', 'assets/tiles/vehicles.png')
-    //this.load.image("tiles", "assets/drawtiles-spaced.png");
-    this.load.image('player', "assets/menus.brick.jpg")
-    //this.load.tilemapCSV("map", "assets/grid.csv");
     this.load.tilemapTiledJSON('map', 'assets/tiles/map-01.json')
     this.load.atlas('fauna', 'assets/sprites/fauna.png', 'assets/sprites/fauna.json')
     this.load.audio('lvl1music', 'assets/sounds/space_traveler.ogg')
   }
 
-  create() {
-    this.sound.stopByKey('menumusic');
-    var lvl1music = this.sound.add('lvl1music')
-    var musicConfig = {
-      mute: false,
-      volume: 1,
-      rate: 1,
-      detune: 0,
-      seek: 0,
-      loop: true,
-      delay: 0
-    }
-    lvl1music.play(musicConfig)
-    
-    // add background
-    //this.add.image(400, 400, "level-1-bkgrd");
+    create() {
+      this.sound.stopByKey('menumusic');
+      var lvl1music = this.sound.add('lvl1music')
+      var musicConfig = {
+        mute: false,
+        volume: 1,
+        rate: 1,
+        detune: 0,
+        seek: 0,
+        loop: true,
+        delay: 0
+      }
+      lvl1music.play(musicConfig)
+
+    // setup map with tiles
     const tilemap = this.make.tilemap({ key: 'map' })
     const tileset1 = tilemap.addTilesetImage('outdoors', 'outdoor-tiles')
     const tileset2 = tilemap.addTilesetImage('vehicles', 'vehicle-tiles')
+    const allLayers = [tileset1, tileset2]
 
     tilemap.createLayer('Ground', tileset1)
-    const bushLayer = tilemap.createLayer('Bushes', tileset1)
+    const bushLayer = tilemap.createLayer('Bushes', allLayers)
     bushLayer.setCollisionByProperty({ collides: true })
     const topLayer1 = tilemap.createLayer('Top', tileset1)
     topLayer1.setCollisionByProperty({ collides: true })
-    const topLayer2 = tilemap.createLayer('Vehicles', tileset2)
-    topLayer2.setCollisionByProperty({ collides: true })
 
+    // setup fauna
     this.fauna = this.physics.add.sprite(140, 140, 'fauna', 'walk-down-3.png')
     this.fauna.body.setSize(this.fauna.width * 0.5, this.fauna.height * 0.3)
     this.fauna.body.setOffset(8, 20)
@@ -89,48 +108,126 @@ export default class LevelScene extends Phaser.Scene {
       frameRate: 10
     })
 
-    this.fauna.anims.play('char-idle-down')
+    this.fauna.anims.play('char-idle-side')
+    // add go button
+    var go = this.add.image(60, 60, 'go')
+    go.setDisplaySize(60,60)
+    go.setInteractive().on('pointerdown', ()=>this.readBlocks(bushLayer));
+    
+    // add test draggable blocks
+    components.DraggableBlock(20, 350, 'right1', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(60, 350, 'right2', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(100, 350, 'right3', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(140, 350, 'left1', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(180, 350, 'left2', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(220, 350, 'left3', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(20, 300, 'forward1', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(60, 300, 'forward2', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(100, 300, 'forward3', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(140, 300, 'forward4', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(180, 300, 'forward5', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(220, 300, 'forward6', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(260, 300, 'forward7', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(300, 300, 'forward8', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(340, 300, 'forward9', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(380, 300, 'forward10', this, {width: 25, height: 25}, this.blockMap);
+    components.DraggableBlock(260, 350, 'down', this, {width: 25, height: 25}, this.blockMap);
+
+    //add whenGo button
+    components.DraggableBlock(300, 350, 'whenGo', this, {width: 25, height: 25}, this.blockMap);
 
     this.physics.add.collider(this.fauna, bushLayer)
     this.physics.add.collider(this.fauna, topLayer1)
-    this.physics.add.collider(this.fauna, topLayer2)
-
-    this.cameras.main.startFollow(this.fauna, true)
-
-    // add test draggable blocks
-    components.DraggableBlock(200, 200, "move-east", this);
-    components.DraggableBlock(200, 200, "move-east", this);
-
+    
+    // add score
+    this.scoreText = this.add.text(310, 16, 'Score: 0', {
+      stroke: '#00000',
+      strokeThickness: 10,
+      fontSize: '12px',
+		})
+    this.movesLeft = this.add.text(305, 40, 'Moves: 10', {
+      stroke: '#00000',
+      strokeThickness: 10,
+      fontSize: '12px',
+		});
   }
 
-  update(t: number, dt: number) {
-    if (!this.cursors || !this.fauna) {
-      return
+  async readBlocks(layer: Phaser.Tilemaps.TilemapLayer){
+    //console.log(this.blockMap)
+    let sortedArr: Array<any> = [];
+    for(let key of this.blockMap.keys()) {
+        sortedArr.push([this.blockMap.get(key)![0], this.blockMap.get(key)![1], key]) 
     }
+    
+    //Sorts array based on x position of the blocks. Sorts blocks in ascending order from lowest
+    //x position to highest (from left to right on screen)
+    sortedArr = sortedArr.sort(function(a, b) { return a[0] - b[0]; })
+    console.log(sortedArr)
 
-    const speed = 100
+    for(let i = 0; i<sortedArr.length; i++){
+      let direction = sortedArr[i][2].slice(0, 4)
+      console.log(direction)
+      console.log(this.fauna.angle);
 
-    if (this.cursors.left?.isDown) {
-      this.fauna.anims.play('char-run-side', true)
-      this.fauna.setVelocity(-speed, 0)
-      this.fauna.scaleX = -1
-      this.fauna.body.offset.x = 24
-    } else if (this.cursors.right?.isDown) {
-      this.fauna.anims.play('char-run-side', true)
-      this.fauna.setVelocity(speed, 0)
-      this.fauna.scaleX = 1
-      this.fauna.body.offset.x = 8
-    } else if (this.cursors.up?.isDown) {
-      this.fauna.anims.play('char-run-up', true)
-      this.fauna.setVelocity(0, -speed)
-    } else if (this.cursors.down?.isDown) {
-      this.fauna.anims.play('char-run-down', true)
-      this.fauna.setVelocity(0, speed)
-    } else {
-      const parts = this.fauna.anims.currentAnim.key.split('-')
-      parts[1] = 'idle'
-      this.fauna.play(parts.join('-'))
-      this.fauna.setVelocity(0, 0)
+
+      const sleep = (ms: number | undefined) => new Promise(r => setTimeout(r, ms));
+      await sleep(500);      
+
+      if(direction === "forw"){
+        if(this.fauna.angle === 0){
+          var tile = layer.getTileAtWorldXY(this.fauna.x + 32, this.fauna.y, true);
+          if (tile.index > 0) {
+            console.log('blocked')
+          }
+            else {
+              this.fauna.anims.play('char-run-side', true)
+              this.fauna.scaleX = 1
+              this.fauna.x += 32;
+            }
+          }
+        else if((this.fauna.angle === 90) || (this.fauna.angle === -270)){
+          var tile = layer.getTileAtWorldXY(this.fauna.x, this.fauna.y + 32, true);
+          if (tile.index > 0) {
+            console.log('blocked')
+          }
+            else {
+              this.fauna.anims.play('char-run-down', true)
+              this.fauna.y += 32;
+            }
+        }
+        else if(Math.abs(this.fauna.angle) === 180){
+          var tile = layer.getTileAtWorldXY(this.fauna.x - 32, this.fauna.y, true);
+          if (tile.index > 0) {
+            console.log('blocked')
+          }
+            else {
+              this.fauna.anims.play('char-run-side', true)
+              this.fauna.scaleX = 1
+              this.fauna.x -= 32;
+            }
+        }
+        else if((this.fauna.angle === 270) || (this.fauna.angle === -90)){
+          var tile = layer.getTileAtWorldXY(this.fauna.x, this.fauna.y - 32, true);
+          if (tile.index > 0) {
+            console.log('blocked')
+          }
+            else {
+              this.fauna.anims.play('char-run-up', true)
+              this.fauna.y -= 32;
+            }
+        }
+      }
+      else if(direction === 'righ'){
+        console.log("rotated right")
+        this.fauna.anims.play('char-idle-down')
+        this.fauna.angle += 90;
+      }
+      else if(direction === 'left'){
+        console.log("rotated left")
+        this.fauna.anims.play('char-idle-up')
+        this.fauna.angle -= 90;
+      }
     }
+    this.fauna.anims.stop()
   }
 }
